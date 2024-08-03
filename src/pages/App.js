0a976deb339ca5884c41f9159c04b4df62517a9f
dragon15 <LIULONG1922615@outlook.com>
2024-08-03 20:09:01 +0800

    0803commit1

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {Routes,Route,Link} from "react-router-dom";
 function App() {
   return (
     <div className="App">
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Routes>
+          <Route path='login' element={}></Route>
+        </Routes>
         <a
           className="App-link"
           href="https://reactjs.org"
